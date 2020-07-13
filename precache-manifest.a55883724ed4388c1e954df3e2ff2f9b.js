a695902323bd696beb90dc47f1dbf4edec0875f5
Gaurav Pendharkar <gauravpendharkar2@gmail.com>
2020-07-13 14:43:23 +0530

    Updates

---
+++ precache-manifest.a55883724ed4388c1e954df3e2ff2f9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d7ea3d71fcc7f71cbd6336a93144cc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9f47219f22bcd466edac",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "423a5cd19004952f74f0",
+    "url": "./static/js/2.4b3ef541.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.4b3ef541.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f47219f22bcd466edac",
+    "url": "./static/js/main.8aa05b8d.chunk.js"
+  },
+  {
+    "revision": "bab6efa3a1b13f60788a",
+    "url": "./static/js/runtime-main.6dc68c31.js"
+  }
+]);
